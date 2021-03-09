b00ce8392bd2a61deb98c95fc7d43575fd3ddff2
Kahra029 <smalloriginals@gmail.com>
2021-03-09 23:41:52 +0900

    main

---
+++ pages/posts/[id].js
@@ -0,0 +1,16 @@
+import Layout from '../../components/layout'
+
+export default function Post(){
+    return(
+        <Layout home>
+
+        </Layout>
+    )
+}
+
+export async function getStaticPaths(){
+
+}
+export async function getStaticProps({ params }){
+    
+}
